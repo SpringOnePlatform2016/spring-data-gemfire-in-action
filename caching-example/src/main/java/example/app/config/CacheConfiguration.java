9cb4ce839f7bd038e8d962857c83577520d6788d
lshannon <luke.shannon@gmail.com>
2016-07-12 17:02:43 -0700

    Updated config to work on PCF

--- caching-example/src/main/java/example/app/config/CacheConfiguration.java
+++ caching-example/src/main/java/example/app/config/CacheConfiguration.java
@@ -1,27 +1,68 @@
 package example.app.config;
 
+import io.pivotal.spring.cloud.service.gemfire.GemfireServiceConnectorConfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.ServiceConnectorConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.gemfire.support.GemfireCacheManager;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
 
-import com.gemstone.gemfire.cache.Cache;
+import com.gemstone.gemfire.cache.client.ClientCache;
+import com.gemstone.gemfire.cache.client.ClientRegionShortcut;
 
+import example.app.model.Customer;
 import example.app.model.CustomerKeyGenerator;
 
 @Configuration
+@Profile({ "cloud" })
 public class CacheConfiguration extends AbstractCloudConfig {
+	
+	@Value("${cache_service_name}")
+	private String cache_service_name;
+	
+	@Value("${region_name}")
+	private String region_name;
+	
+	/**
+	 * This is where the Cache behavior is configured
+	 * @return ServiceConnectorConfig
+	 */
+	public ServiceConnectorConfig createGemfireConnectorConfig() {
+	    GemfireServiceConnectorConfig gemfireConfig = new GemfireServiceConnectorConfig();
+	    gemfireConfig.setPoolIdleTimeout(7777L);
+	    //gemfireConfig.setPdxSerializer(new ReflectionBasedAutoSerializer(pdx_class));
+	    gemfireConfig.setPdxPersistent(true);
+	    return gemfireConfig;
+	  }
+
 
 	@Bean
-    GemfireCacheManager cacheManager(final Cache gemfireCache) {
-        GemfireCacheManager cacheManager = new GemfireCacheManager();
-        cacheManager.setCache(gemfireCache);
-        return cacheManager;
-    }
-	
-	@Bean(name="keyGenerator")
+	ClientCache clientCache() {
+		CloudFactory cloudFactory = new CloudFactory();
+		Cloud cloud = cloudFactory.getCloud();
+	    ClientCache cache = cloud.getServiceConnector(cache_service_name, ClientCache.class, createGemfireConnectorConfig());
+		return cache;
+	}
+
+	@Bean
+	public ClientRegionFactoryBean<String, Customer> personRegion(ClientCache cache) {
+		ClientRegionFactoryBean<String,Customer> region = new
+		ClientRegionFactoryBean<>();
+		region.setCache(cache);
+		region.setName(region_name);
+		region.setShortcut(ClientRegionShortcut.PROXY);
+		return region;
+
+	}
+
+	@Bean(name = "keyGenerator")
 	CustomerKeyGenerator getKeyGenerator() {
 		return new CustomerKeyGenerator();
 	}
-	
+
 }
---
+++ cluster-troubleshoot-example/README.md
@@ -0,0 +1,17 @@
+# Troubleshooting A PCF Gemfire cluster
+
+## Getting Artifacts From The members
+
+```shell
+
+
+
+```
+
+## Resetting The cluster
+
+```shell
+
+cf restart-gemfire customer-cluster --reset-defaults
+
+```
