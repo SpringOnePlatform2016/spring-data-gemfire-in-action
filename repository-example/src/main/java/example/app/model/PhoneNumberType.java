ae1cf6ba00da8aa1e749964b9a71ca2a47e23f34
John Blum <jblum@pivotal.io>
2016-07-21 11:45:31 -0700

    Create AddressType and PhoneNumberType enumerated types to categorize addresses and phone numbers.

---
+++ repository-example/src/main/java/example/app/model/PhoneNumberType.java
@@ -0,0 +1,19 @@
+package example.app.model;
+
+/**
+ * The PhoneNumberType enum is an enumeration of different {@link PhoneNumber} types.
+ *
+ * @author John Blum
+ * @see example.app.model.PhoneNumber
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public enum PhoneNumberType {
+	BUSINESS,
+	IP,
+	HOME,
+	MOBILE,
+	OFFICE,
+	WORK
+
+}
